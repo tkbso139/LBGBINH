adc36322749e53df0b7ac6173de5e47ab2c43b7c
TKBSO139 <tkbso139@gmail.com>
2026-09-19 05:14:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_ACTIONS ? '/TKB/' : './',
+    base:  '/LBGBINH/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
